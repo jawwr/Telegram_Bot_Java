2656b54061e5c1de063c84528b5f8bfa476e58f1
Arseny <20mineslend14@gmail.com>
2022-03-24 22:41:34 +0500

    deploy

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -1,12 +1,15 @@
 package bot;
 
+import bot.controller.BotController;
 import bot.telegram_bot.MyTelegramBot;
+import org.springframework.boot.SpringApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(BotController.class);
         try {
             TelegramBotsApi bot = new TelegramBotsApi(DefaultBotSession.class);
             bot.registerBot(new MyTelegramBot());
--- src/main/java/bot/controller/BotController.java
+++ src/main/java/bot/controller/BotController.java
@@ -1,11 +1,11 @@
 package bot.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BotController {
 
     @RequestMapping("/")
