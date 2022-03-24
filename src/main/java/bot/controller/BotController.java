bdbc11023e57c40ba19a2bc9c42067ac67bf32d8
Arseny <20mineslend14@gmail.com>
2022-03-24 22:32:59 +0500

    get

--- src/main/java/bot/controller/BotController.java
+++ src/main/java/bot/controller/BotController.java
@@ -2,13 +2,15 @@ package bot.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BotController {
 
-    @GetMapping("/")
-    public String main(){
-        return "main";
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 }
